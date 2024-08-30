f7c273090462d00ee2beea4116ea80ce600a43ec
nikachello <nikain35@gmail.com>
2024-08-31 03:22:42 +0400

    updated logs

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ const app = express();
 app.get("/hangup", (req, res) => {
   const { reason, userID } = req.query;
   console.log(`Hangup: Reason = ${reason}, userID=${userID}`);
-  res.status(200);
+  res.status(200).send(`Hangup: Reason = ${reason}, userID=${userID}`);
 });
 
 app.get("/events", (req, res) => {
   const { action, userID } = req.query;
   console.log(`Event: Action = ${action}, userID  = ${userID}`);
-  res.status(200);
+  res.status(200).send(`Event: Action = ${action}, userID  = ${userID}`);
 });
 
 app.get("/answer", (req, res) => {
   const { userID } = req.query;
   console.log(`Answer: UserID = ${userID}`);
-  res.sendStatus(200);
+  res.status(200).send(`Answer: UserID = ${userID}`);
 });
 
 app.listen(3000, () => {
