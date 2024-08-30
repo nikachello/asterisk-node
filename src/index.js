a95aa94080f1db61507df1b29e44150e23040557
nikachello <nikain35@gmail.com>
2024-08-31 03:26:46 +0400

    updated logs

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ const express = require("express");
 const app = express();
 
 app.get("/hangup", (req, res) => {
-  const { reason, userID } = req.query;
-  console.log(`Hangup: Reason = ${reason}, userID=${userID}`);
-  res.status(200).send(`Hangup: Reason = ${reason}, userID=${userID}`);
+  const { reason, userid } = req.query;
+  console.log(`Hangup: Reason = ${reason}, userID=${userid}`);
+  res.status(200).send(`Hangup: Reason = ${reason}, userID=${userid}`);
 });
 
 app.get("/events", (req, res) => {
-  const { action, userID } = req.query;
-  console.log(`Event: Action = ${action}, userID  = ${userID}`);
-  res.status(200).send(`Event: Action = ${action}, userID  = ${userID}`);
+  const { action, userid } = req.query;
+  console.log(`Event: Action = ${action}, userID  = ${userid}`);
+  res.status(200).send(`Event: Action = ${action}, userID  = ${userid}`);
 });
 
 app.get("/answer", (req, res) => {
-  const { userID } = req.query;
-  console.log(`Answer: UserID = ${userID}`);
-  res.status(200).send(`Answer: UserID = ${userID}`);
+  const { userid } = req.query;
+  console.log(`Answer: UserID = ${userid}`);
+  res.status(200).send(`Answer: UserID = ${userid}`);
 });
 
 app.listen(3000, () => {
