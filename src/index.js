1db7df681d922849551f7778774e8a1a3e47702d
nikachello <nikain35@gmail.com>
2024-08-31 03:51:17 +0400

    updated logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
 const app = express();
+const TELEGRAM_API_TOKEN = "7227645715:AAE9fMuy-9oQ2wtlKc6Kx9TrcfjTmYBs8vo";
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}`;
+
+app.use(bodyParser.json());
+
+app.post("/webhook", async (req, res) => {
+  const message = req.body.message;
+
+  if (message && message.text) {
+    const chatId = message.chat.id;
+    const text = message.text;
+
+    await sendMessage(chatId, `You texted me: ${text}`);
+  }
+
+  res.status(200);
+});
+
+async function sendMessage(chatId, text) {
+  try {
+    await axios.post(`${TELEGRAM_API_URL}/sendMessage`, {
+      chat_id: chatId,
+      text: text,
+    });
+  } catch (error) {
+    console.error("Error sending message", error);
+  }
+}
 
 app.get("/hangup", (req, res) => {
   const { reason, userid } = req.query;
