cf37aed4235a6421371341489e2345d92496bbb1
nikachello <nikain35@gmail.com>
2024-08-31 03:09:06 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/hangup", (req, res) => {
+  const { reason, userID } = req.query;
+  console.log(`Hangup: Reason = ${reason}, userID=${userID}`);
+  res.status(200);
+});
+
+app.get("/events", (req, res) => {
+  const { action, userID } = req.query;
+  console.log(`Event: Action = ${action}, userID  = ${userID}`);
+  res.status(200);
+});
+
+app.get("/answer", (req, res) => {
+  const { userID } = req.query;
+  console.log(`Answer: UserID = ${userID}`);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
